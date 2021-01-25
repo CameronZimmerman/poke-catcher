b4c6036302b800fb287d1f9a8cc16453ac6d108f
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-25 15:39:43 -0800

    add updatePokeStats function

---
+++ local-storage-utils.js
@@ -0,0 +1,26 @@
+const POKESTATS = 'POKESTATS';
+
+export function getPokeStats(){
+    return JSON.parse(localStorage.getItem(POKESTATS) || '[]');
+}
+
+export function updatePokeStats(pokemon, isCaught) {
+    const pokeStats = getPokeStats();
+    const pokemonStat = findById(pokemon);
+
+    if (!isCaught) {
+
+        if (pokemonStat) pokemonStat.seen++;
+        else {
+            const newPokeStat = {
+                _id : pokemon._id,
+                seen : 1,
+                caught : 0
+            };
+
+            pokeStats.push(newPokeStat);
+        }
+    }
+
+    localStorage.setItem(POKESTATS, JSON.stringify(pokeStats));
+}
